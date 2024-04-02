23377cf25da86b5fd9f0df517414e87b89e85be0
Sudhakar Anemu <sudhakar.anemu@gmail.com>
2024-04-02 17:54:25 -0400

    added

---
+++ nginx.sh
@@ -0,0 +1,28 @@
+#js 
+
+1. Installation of Nginx on Ubuntu
+
+https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-22-04
+
+root@ip-172-31-80-233:~# ufw status
+Status: active
+
+To                         Action      From
+--                         ------      ----
+Nginx HTTP                 ALLOW       Anywhere
+Nginx HTTP (v6)            ALLOW       Anywhere (v6)
+
+root@ip-172-31-80-233:~#
+
+---> Below 
+
+root@ip-172-31-80-233:/var/www/html# pwd
+/var/www/html
+root@ip-172-31-80-233:/var/www/html# ls -l
+total 8
+-rw-r--r-- 1 root root 612 Apr  2 20:34 bkindex.nginx-debian.html
+-rw-r--r-- 1 root root 628 Apr  2 20:34 index.nginx-debian.html
+root@ip-172-31-80-233:/var/www/html#
+
+
+
