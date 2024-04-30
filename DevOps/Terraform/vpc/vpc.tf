3c39c6ba41df195e97e90b91945a8d59bdbbdff8
Sudhakar Anemu <sudhakar.anemu@gmail.com>
2024-04-30 16:40:30 -0400

    add tf

--- DevOps/Terraform/vpc/vpc.tf
+++ DevOps/Terraform/vpc/vpc.tf
@@ -2,6 +2,6 @@ resource "aws_vpc" "main-vpc" {
     cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = "true"
     tags = {
-        Name = "amit.01-vpc"
+        Name = "jssudha.test-vpc"
     }
 }
